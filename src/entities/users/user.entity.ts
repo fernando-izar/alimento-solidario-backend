0601f56f8359200463d32d35652c039a479ae8e9
alexfarias1974 <alex_wfariaas@yahoo.com.br>
2022-10-30 12:59:47 -0400

    feat: user and address entities + user interface

---
+++ src/entities/users/user.entity.ts
@@ -0,0 +1,38 @@
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from "typeorm";
+import { Exclude } from "class-transformer";
+import { Address } from "../addresses/address.entity";
+
+@Entity("users")
+export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column({unique: true})
+    email: string
+
+    @Column()
+    @Exclude()
+    password: string
+
+    @Column()
+    name: string
+
+    @Column()
+    cnpj_cpf: string
+
+    @Column()
+    responsible: string
+
+    @Column()
+    contact: string
+
+    @Column()
+    type: string
+
+    @Column()
+    isAdm: boolean
+
+    @OneToOne(() => Address)
+    @JoinColumn()
+    address: Address
+}
