f03205b5177366a2822a23fd229a39d43477616e
alexfarias1974 <alex_wfariaas@yahoo.com.br>
2022-11-01 14:50:03 -0400

    fix: status code ensureIsAdmMiddleware

--- src/middlewares/ensureIsAdm.middleware.ts
+++ src/middlewares/ensureIsAdm.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 const ensureIsAdmMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     if (!req.user.isAdm) {
-        return res.status(401).json({
+        return res.status(403).json({
             message: "User is not admin!"
         })
     }
