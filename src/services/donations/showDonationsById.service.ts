b4c1a7f95d111830717d6c94bfd6e65ac38ee6c9
Leonardo Kratz Mendes <kratzmendes@gmail.com>
2022-11-03 17:31:29 -0300

    feat: showDonationsById

---
+++ src/services/donations/showDonationsById.service.ts
@@ -0,0 +1,20 @@
+import AppDataSource from "../../data-source";
+import { Donation } from "../../entities/donations.entity";
+import AppError from "../../errors/appError";
+
+const showDonationsByIdService = async (id: string): Promise<Donation> => {
+  const donationsRepository = AppDataSource.getRepository(Donation);
+  const donationFound = await donationsRepository.findOne({
+    where: {
+      id,
+    },
+  });
+
+  if (!donationFound) {
+    throw new AppError("User not found!");
+  }
+
+  return donationFound;
+};
+
+export default showDonationsByIdService;
