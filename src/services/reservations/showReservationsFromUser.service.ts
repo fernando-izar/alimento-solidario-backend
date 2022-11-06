1d824cb499eff57af36d7bbc1ad7c1702b24bf19
fernando-izar <fizar@me.com>
2022-11-06 06:34:28 -0300

    fix: show user and not show password and cnpj

--- src/services/reservations/showReservationsFromUser.service.ts
+++ src/services/reservations/showReservationsFromUser.service.ts
@@ -2,25 +2,28 @@ import AppDataSource from "../../data-source";
 import { Reservation } from "../../entities/reservations.entity";
 import { User } from "../../entities/user.entity";
 
-const showReservationsService = async (userId: string): Promise<Reservation[]> => {
+const showReservationsService = async (
+  userId: string
+): Promise<Reservation[]> => {
   const reservationsRepository = AppDataSource.getRepository(Reservation);
-  const usersRepository = AppDataSource.getRepository(User)
+  const usersRepository = AppDataSource.getRepository(User);
 
   const user = await usersRepository.findOneBy({
-    id: userId
-  })
+    id: userId,
+  });
 
   const reservations = await reservationsRepository.find({
     where: {
       user: {
-        id: user?.id
-      }      
+        id: user?.id,
+      },
     },
     relations: {
       donation: {
-        user: true
-      }
-    }
+        user: true,
+      },
+      user: true,
+    },
   });
 
   return reservations;
