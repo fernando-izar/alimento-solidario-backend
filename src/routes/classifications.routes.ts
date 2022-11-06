14f2ea66c17616a22998c60e0c4faa3d1948fecb
fernando-izar <fizar@me.com>
2022-11-06 15:44:11 -0300

    feat show classification id by name

--- src/routes/classifications.routes.ts
+++ src/routes/classifications.routes.ts
@@ -2,11 +2,18 @@ import { Router } from "express";
 import createClassificationController from "../controllers/classifications/createClassification.controller";
 import deleteClassificationController from "../controllers/classifications/deleteClassification.controller";
 import { listClassificationsController } from "../controllers/classifications/listClassifications.controller";
+import { showClassificationIdByNameController } from "../controllers/classifications/showClassificationIdByName.controller";
 import updateClassificationController from "../controllers/classifications/updateClassification.controller";
 import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
 import ensureIsAdmMiddleware from "../middlewares/ensureIsAdm.middleware";
 
 const classificationRoutes = Router();
+
+classificationRoutes.get(
+  "/name/:name",
+  ensureAuthMiddleware,
+  showClassificationIdByNameController
+);
 
 classificationRoutes.get(
   "",
